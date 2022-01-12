d88fec3b55700f03479736924203fe55e9654bcf
Rafik Paimin <rafikpaimin@gmail.com>
2022-01-12 19:54:04 +0800

    Ghost Updates (WIP)
    - Added ghost model
    - Moved props model to props folder

---
+++ Assets/Scripts/ScriptableObjects/SO_Ghosts.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Sirenix.OdinInspector;
+
+[System.Serializable]
+public struct GhostDetails{
+    public string name;
+    [PreviewField] public Sprite avatarIcon;
+    public GameObject fullbodyPrefab;
+    public GameObject handPrefab;
+    public float cameraHeight;
+}
+
+
+[CreateAssetMenu(fileName = "New Ghost", menuName = "Database/Ghost")]
+public class SO_Ghosts : ScriptableObject
+{
+    [TableList(ShowIndexLabels = true)]public List<GhostDetails> ghostLists;
+    
+}
