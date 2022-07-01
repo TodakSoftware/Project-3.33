d6593d52fefb38eca5865da99549ee70176bdf45
Rafik Paimin <rafik@todak.com>
2022-07-01 10:05:17 +0800

    Player updates

--- Assets/Scripts/Gameplay/PlayerInteraction.cs
+++ Assets/Scripts/Gameplay/PlayerInteraction.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
-    
+    public Transform cam;
+    public float interactDistance;
+    bool interactable;
+
+    void Update(){
+        RaycastHit hit;
+        interactable = Physics.Raycast(cam.position, cam.TransformDirection(Vector3.forward), out hit, interactDistance);
+
+        if(interactable && Input.GetButtonDown("Interact")){
+            print(hit.collider.gameObject.name);
+        }
+    }
 }
