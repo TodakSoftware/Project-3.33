f3185df3823e2a1b1df779c59cb90afa8ded7219
Rafik Paimin <rafik@todak.com>
2022-07-04 00:25:34 +0800

    Human Win update ui placeholder

---
+++ Assets/Scripts/Gameplay/PlayerUI.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour
+{
+    public UI_VictoryResult uiVictoryResult;
+    
+}
