f05cd8ffb258f7ca8feecd4d086661d76cd7ea05
muhaimin117 <46334522+muhaimin117@users.noreply.github.com>
2022-07-15 19:15:06 +0800

    map update
    map update

--- Assets/Scripts/FXS/tower_pulsating_light.cs
+++ Assets/Scripts/FXS/tower_pulsating_light.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class tower_pulsating_light : MonoBehaviour
 {
+
+    private Animation towerLight;
     // Start is called before the first frame update
     void Start()
     {
+        if(towerLight !=null)
+        towerLight.Play();
         
     }
 
