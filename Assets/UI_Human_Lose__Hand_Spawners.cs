d478efb0520ad47f971ebccbf503d4be4c1510af
Azizi Aziz <84830123+HiromuDesu@users.noreply.github.com>
2022-08-11 18:38:51 +0800

    Win Lose Update
    bruh

--- Assets/UI_Human_Lose__Hand_Spawners.cs
+++ Assets/UI_Human_Lose__Hand_Spawners.cs
@@ -13,6 +13,8 @@ public class UI_Human_Lose__Hand_Spawners : MonoBehaviour
     [SerializeField]
     private int SpawnCurrentNumber;
     public int SpawnLimit;
+    
+    [Space]
 
     [Header("Spawn Frequencies")]
     [Tooltip("The minimum amount of object to spawn in X")]
@@ -25,10 +27,24 @@ public class UI_Human_Lose__Hand_Spawners : MonoBehaviour
     [Tooltip("The maximum amount of object to spawn in Y")]
     public int SpawnFrequencyYMax;
 
+    [Space]
+
+    [Header("Positionings")]
+    [Tooltip("The minimum amount of object to spawn in X")]
+    public int SpawnRowNumber;
+    [Tooltip("The maximum amount of object to spawn in X")]
+    public int SpawnColumnNumber;
+
+
+
+    [Space]
+
     [Header("Spawn Rotations")]
     [Tooltip("The amount of angles to rotate the image")]
     public int SpawnRotationMinRange;
     public int SpawnRotationMaxRange;
+
+    [Space]
 
     [Header("Timings")]
     [Tooltip("How long to wait before the next spawn")]
@@ -68,13 +84,37 @@ public class UI_Human_Lose__Hand_Spawners : MonoBehaviour
         Debug.Log("Screen Resolution: "+ Screen.currentResolution);
         while (SpawnLimit != SpawnCurrentNumber)
         {
-            Instantiate(SpawningImagePrefab, new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), 0), Quaternion.identity, ParentObject.transform);
+            Instantiate(SpawningImagePrefab, new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), 0), Quaternion.Euler(0, 0, Random.Range(SpawnRotationMinRange, SpawnRotationMaxRange)), ParentObject.transform);
             SpawnCurrentNumber++;
         }
 
         SpawnCurrentNumber = 0;
     }
 
+    //delay version
+    IEnumerator SpawnTheImageDelayVersionIEnumerator(int TotalImages, int RowNumberHere, float SpawnDelayHere)
+    {
+        //for checking rows
+        for (int CurrentRowNumberHere = 0; CurrentRowNumberHere < RowNumberHere; CurrentRowNumberHere++)
+        {
+            //for spawning
+            for (int i = 0; i < (TotalImages / RowNumberHere); i++)
+            {
+                Instantiate(SpawningImagePrefab, new Vector3(Random.Range(0, Screen.width), Random.Range(CurrentRowNumberHere * (Screen.height / RowNumberHere), (CurrentRowNumberHere + 1) * (Screen.height / RowNumberHere)), 0), Quaternion.Euler(0, 0, Random.Range(SpawnRotationMinRange, SpawnRotationMaxRange)), ParentObject.transform);
+                yield return new WaitForSeconds(SpawnDelayHere);
+            }
 
+        }
+            
 
+    }
+
+    //(Screen.height / SpawnRowNumber) = 
+
+    public void SpawnTheImageDelayVersion()
+    {
+
+        StartCoroutine(SpawnTheImageDelayVersionIEnumerator(SpawnLimit, SpawnRowNumber, SpawnDelay));
+
+    }
 }
