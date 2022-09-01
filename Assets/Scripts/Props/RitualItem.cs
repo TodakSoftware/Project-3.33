0c0f746df746b4ba9d0e440a73d32b512840cf2d
Rafik Paimin <rafik@todak.com>
2022-09-01 15:49:57 +0800

    Ritual highlight fixed

--- Assets/Scripts/Props/RitualItem.cs
+++ Assets/Scripts/Props/RitualItem.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
+using DG.Tweening;
 
 [RequireComponent(typeof(Interactable))]
 public class RitualItem : MonoBehaviourPunCallbacks
@@ -12,6 +13,10 @@ public class RitualItem : MonoBehaviourPunCallbacks
     [Header("Item Info")]
     public SO_RitualItem ritualItemSO;
     public string code;
+    [Header("Material Highlight")]
+    public List<MeshRenderer> meshRendererList = new List<MeshRenderer>();
+    List<Material> materialList = new List<Material>();
+    bool isHighlighted;
 
     public void Start(){
         foreach(var item in ritualItemSO.ritualItemLists){
@@ -26,12 +31,42 @@ public class RitualItem : MonoBehaviourPunCallbacks
                 gameObject.name = item.displayName; // set item name
             } // end if(item.code == code)
         } // end foreach
+
+        if(meshRendererList.Count > 0){
+            foreach(var mat in meshRendererList){
+                materialList.Add(mat.sharedMaterial);
+            }
+        }
     }// end Start
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.H)){
+            HighlightItem();
+        }
+    }
 
     [PunRPC]
     void DestroyItem(){
         if(photonView.IsMine){
             PhotonNetwork.Destroy(gameObject);
+        }
+    }
+
+    void HighlightItem(){
+        foreach(var a in materialList){
+            print(a.color);
+            
+            //a.SetColor("_EmissiveColor", Color.white * 12f);
+            //a.DOFloat(12,"_EmissiveColor",1f);
+            a.DOVector(Color.white * 12f, "_EmissiveColor", 1f).SetLoops(-1, LoopType.Yoyo);
+        }
+    }
+
+    private void OnApplicationQuit() {
+        foreach(var a in materialList){
+            print(a.color);
+            
+            a.SetColor("_EmissiveColor", Color.white * 1f);
         }
     }
 }
