6cff02a1d8dd56f3dde7eeb13a824005878a7c31
Rafik Paimin <rafik@todak.com>
2022-09-02 11:54:16 +0800

    Revert "Footstep functionality"
    This reverts commit e49f219f1289cb8441492dbab737ded53fbac7d3.

--- Assets/Scripts/Class/C_AudioData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-// RITUAL ITEMS DETAILS used in SO_RitualItem
-[System.Serializable]
-public class C_AudioData{
-    public string name;
-    public List<AudioClip> audioClipLists = new List<AudioClip>();
-}
