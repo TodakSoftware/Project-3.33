e49f219f1289cb8441492dbab737ded53fbac7d3
Rafik Paimin <rafik@todak.com>
2022-09-02 11:45:20 +0800

    Footstep functionality

---
+++ Assets/Scripts/Class/C_AudioData.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+// RITUAL ITEMS DETAILS used in SO_RitualItem
+[System.Serializable]
+public class C_AudioData{
+    public string name;
+    public List<AudioClip> audioClipLists = new List<AudioClip>();
+}
