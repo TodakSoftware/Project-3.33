ebdfcb4c6015942f9d771cacec923348db97a2c4
amtodak <amtodak98@gmail.com>
2022-09-09 18:04:37 +0800

    Update Wall Charger
    In Progress

--- Assets/Scripts/Props/WallCharger.cs
+++ Assets/Scripts/Props/WallCharger.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using DG.Tweening;
+
 
 public class WallCharger : MonoBehaviour
 {
+    Interactable interactable;
+    //[SerializeField] GameObject interactableScript;
+    
+    public bool isBatteryAvailable;
+    public int wallChargerPercent;
+    
+    void Awake()
+    {
+       //interactable = interactableScript.GetComponent<Interactable>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        //this is wall charger script
+        wallChargerPercent = Random.Range(0, 100);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //test foreal
+         if(Input.GetKeyDown(KeyCode.F)){
+            ChargePhone();
+        }
+    }
+
+    public void ChargePhone()
+    {
+        wallChargerPercent -= 10;
+        Debug.Log(wallChargerPercent + " Left");
     }
 }
