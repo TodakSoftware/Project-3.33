3c1bd0291a8eeeedf871ecbee699de4eeed2ae60
amtodak <amtodak98@gmail.com>
2022-09-09 15:36:33 +0800

    Update WallCharger.cs
    push test

--- Assets/Scripts/Props/WallCharger.cs
+++ Assets/Scripts/Props/WallCharger.cs
@@ -7,12 +7,12 @@ public class WallCharger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //this is wall charger script
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //test foreal
     }
 }
